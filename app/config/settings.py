4445f966594c93f6e699fb2a517f9ef4e54e9eaa
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2018-01-11 14:42:53 -0300

    [ADD] Suporte à descrição do edital

--- app/config/settings.py
+++ app/config/settings.py
@@ -62,15 +62,15 @@ TEMPLATES = [
 
 DEBUG = True
 SECRET_KEY = 'troqueme'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'localhost',
-        'PORT': 5431,
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
