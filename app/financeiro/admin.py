7872c75b29e1ea78f201735313f8fca058d21024
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2018-01-21 17:25:49 -0300

    * [x] Adiciona ordenação por edital e/ou função. * [x] Adiciona filtro por período de data do empenho.

--- app/financeiro/admin.py
+++ app/financeiro/admin.py
@@ -23,8 +23,29 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 from django.contrib.admin import ModelAdmin, register
 from .models import Pagamento
+from daterange_filter.filter import DateRangeFilter
 
 
 @register(Pagamento)
 class PagamentoAdmin(ModelAdmin):
-    pass
+    date_hierarchy = 'data_empenho'
+    search_fields = ['vinculo__prestador__nome_apresentacao', 'vinculo__prestador__nome_civil',
+                     'vinculo__prestador__nome_social', 'vinculo__prestador__cpf', 'vinculo__prestador__numero_siape', ]
+    list_display = ['id', 'data_empenho', 'valor', 'nome', 'funcao', 'edital', ]
+    list_filter = [('data_empenho', DateRangeFilter), 'vinculo__vaga__edital__identificacao',
+                   'vinculo__vaga__funcao', 'vinculo__vaga__funcao__jornada', 'vinculo__eh_servidor', ]
+    list_select_related = ('vinculo', 'vinculo__prestador',
+                           'vinculo__vaga', 'vinculo__vaga__funcao', 'vinculo__vaga__edital')
+
+    def nome(self, obj):
+        return obj.vinculo.prestador.nome_apresentacao
+    nome.admin_order_field = 'vinculo__prestador__nome_apresentacao'
+
+    def funcao(self, obj):
+        return obj.vinculo.vaga.funcao.nome
+    funcao.admin_order_field = 'vinculo__vaga__funcao__nome'
+    funcao.short_description = 'Função'
+
+    def edital(self, obj):
+        return obj.vinculo.vaga.edital.identificacao
+    edital.admin_order_field = 'vinculo__vaga__edital__identificacao'
