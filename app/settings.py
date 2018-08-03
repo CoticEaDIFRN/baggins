1c0f8940d4c50bbb578c34912fc61be67a802a96
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2018-08-03 19:55:41 -0300

    [UPD] usando o novo modelo

--- app/settings.py
+++ app/settings.py
@@ -1,34 +1,26 @@
 import os
+from python_brfied.env import env, env_as_bool, env_as_list, env_as_list_of_maps
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-def env_or_default(env_name, default_value):
-    return os.environ[env_name] if env_name in os.environ else default_value
+SECRET_KEY = env('DJANGO_SECRET_KEY', 'changeme')
+DEBUG = env_as_bool('DJANGO_DEBUG', True)
+ALLOWED_HOSTS = env_as_list('DJANGO_ALLOWED_HOSTS', '*' if DEBUG else '')
 
+MY_APPS = env_as_list('MY_APPS', 'tipo,contrato,financeiro')
 
-DEBUG = env_or_default('DJANGO_DEBUG', 'True') == 'True'
+DEV_APPS = env_as_list('DEV_APPS', 'django_extensions,django-debug-toolbar') if DEBUG else []
 
-MY_APPS = ('tipo', 'contrato', 'financeiro', )
-THIRD_APPS = ('django_brfied.django_brfied', 'daterange_filter', )
-if DEBUG:
-    DEV_APPS = ('django_extensions', 'debug_toolbar', )
-else:
-    DEV_APPS = tuple()
+THIRD_APPS = env_as_list('THIRD_APPS', 'daterange_filter')
 
-DJANGO_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-)
+DJANGO_APPS = env_as_list('DJANGO_APPS', 'django.contrib.admin,'
+                                         'django.contrib.auth,'
+                                         'django.contrib.contenttypes,'
+                                         'django.contrib.sessions,'
+                                         'django.contrib.messages,'
+                                         'django.contrib.staticfiles')
 
 INSTALLED_APPS = MY_APPS + THIRD_APPS + DEV_APPS + DJANGO_APPS
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-ROOT_URLCONF = 'urls'
-WSGI_APPLICATION = 'wsgi.application'
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -38,11 +30,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
 ]
 
-if DEBUG:
-    MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware', ] + MIDDLEWARE
+ROOT_URLCONF = env('DJANGO_ROOT_URLCONF', 'urls')
 
 TEMPLATES = [
     {
@@ -60,29 +50,29 @@ TEMPLATES = [
     },
 ]
 
-LANGUAGE_CODE = env_or_default('DJANGO_LANGUAGE_CODE', 'pt-br')
-TIME_ZONE = env_or_default('DJANGO_TIME_ZONE', 'UTC')
-USE_I18N = env_or_default('DJANGO_USE_I18N', 'True') == 'True'
-# USE_L10N = env_or_default('DJANGO_USE_L10N', 'True') == 'True'
-USE_TZ = env_or_default('DJANGO_USE_TZ', 'True') == 'True'
-DATE_FORMAT = 'd/b/Y'
-SHORT_DATE_FORMAT = 'd/m/Y'
-
-SECRET_KEY = env_or_default('DJANGO_SECRET_KEY', 'troqueme')
-ALLOWED_HOSTS = env_or_default('DJANGO_SECRET_ALLOWED_HOSTS', '*').split(',')
+WSGI_APPLICATION = env('DJANGO_WSGI_APPLICATION', 'wsgi.application')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': env_or_default('DJANGO_DB_HOST', 'db'),
-        'PORT': int(env_or_default('DJANGO_DB_PORT', '5432'),),
-        'NAME': env_or_default('DJANGO_DB_NAME', 'postgres'),
-        'USER': env_or_default('DJANGO_DB_USER', 'postgres'),
-        'PASSWORD': env_or_default('DJANGO_DB_PASS', 'postgres'),
+        'ENGINE': env('POSTGRES_ENGINE', 'django.db.backends.postgresql_psycopg2'),
+        'HOST': env('POSTGRES_HOST', 'db'),
+        'PORT': env('POSTGRES_PORT', '5432'),
+        'NAME': env('POSTGRES_DB', 'dbname'),
+        'USER': env('POSTGRES_USER', 'username'),
+        'PASSWORD': env('POSTGRES_PASSWORD', 'passw'),
     }
 }
 
-URL_PATH_PREFIX = env_or_default('URL_PATH_PREFIX', 'baggins/')
-LOGIN_URL = '/%saccounts/login/' % URL_PATH_PREFIX
-LOGIN_REDIRECT_URL = '/%saccounts/profile/' % URL_PATH_PREFIX
-STATIC_URL = '/%sstatic/' % URL_PATH_PREFIX
+AUTH_PASSWORD_VALIDATORS = env_as_list_of_maps('DJANGO_UTH_PASSWORD_VALIDATORS', 'NAME', 
+                                               'django.contrib.auth.password_validation.UserAttributeSimilarityValidator,'
+                                               'django.contrib.auth.password_validation.MinimumLengthValidator,'
+                                               'django.contrib.auth.password_validation.CommonPasswordValidator,'
+                                               'django.contrib.auth.password_validation.NumericPasswordValidator')
+
+LANGUAGE_CODE = env('DJANGO_USE_I18N', 'pt-br')
+TIME_ZONE = env('DJANGO_USE_I18N', 'UTC')
+USE_I18N = env_as_bool('DJANGO_USE_I18N', True)
+USE_L10N = env_as_bool('DJANGO_USE_L10N', True)
+USE_TZ = env_as_bool('DJANGO_USE_TZ', True)
+
+STATIC_URL = env('DJANGO_STATIC_URL', '/static/')
